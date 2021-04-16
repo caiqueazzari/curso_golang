aafdb7dd8ad6ac9bdcb592da16fb09fea0abfbda
Caique Azzari <caiquetake@gmail.com>
2021-04-16 17:56:37 -0300

    Updated

--- 008-hello-world/happycat.go
+++ 008-hello-world/happycat.go
@@ -6,8 +6,8 @@ func main() {
 	fmt.Println("Hello! My cat Piki is very cute")
 	potato()
 
-	for i:=0; i < 10; i++ {
-		if i % 2 == 0{
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
 			fmt.Println(i)
 		}
 	}
