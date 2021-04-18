90135cb1cb2c45ca96c18a8d52102a74dcec2d3f
Caique Azzari <caiquetake@gmail.com>
2021-04-18 00:12:21 -0300

    I'm doing great

---
+++ 04_/008-hello-world/happycat.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello! My cat Piki is very cute")
+	potato()
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func potato() {
+	fmt.Println("\nI like potatoes\n ")
+}
