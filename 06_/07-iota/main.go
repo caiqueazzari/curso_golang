6b832c2c291f3b5c0b74097a9b66fc3452ee7cf1
Caique Azzari <caiquetake@gmail.com@users.noreply.github.com>
2021-04-24 15:44:50 -0300

    iota

---
+++ 06_/07-iota/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const (
+	a = iota
+	b
+	c
+        d = iota
+	e
+        f
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+}
