a0b9335059afff542f14befddc15cdfad23b0018
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-04-24 18:40:59 -0300

    Create ex002

--- 07_ninja-level-2/ex002/main.go
+++ 07_ninja-level-2/ex002/main.go
@@ -1,7 +1,24 @@
+/*
+Using the following operators, write expressions and assign their values to variables:
+==
+<=
+>=
+!=
+<
+>
+Now print each of the variables.
+*/
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println()
+	a := (3 == 23)
+	b := (3 <= 23)
+	c := (3 >= 23)
+	d := (3 != 23)
+	e := (3 > 23)
+	f := (3 < 23)
+	fmt.Printf("%v\n%v\n%v\n%v\n%v\n%v\n", a, b, c, d, e, f)
 }
