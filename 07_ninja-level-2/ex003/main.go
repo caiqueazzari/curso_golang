beab2d74176c9f0d6de096e3a554b67489c9f908
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-04-25 12:33:14 -0300

    Create ex003

---
+++ 07_ninja-level-2/ex003/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const (
+	a         = 12
+	b         = 32
+	c int     = 45
+	d float64 = 54.35
+	e string  = "Caique Azzari"
+)
+
+func main() {
+	fmt.Println("Untyped constants:\n")
+	fmt.Printf("a: %v \ttype: %T\n", a, a)
+	fmt.Printf("b: %v \ttype: %T\n", b, b)
+	fmt.Println("\nTyped constants:\n")
+	fmt.Printf("c: %v \ttype: %T\n", c, c)
+	fmt.Printf("d: %v \ttype: %T\n", d, d)
+	fmt.Printf("e: %v \ttype: %T\n", e, e)
+}
