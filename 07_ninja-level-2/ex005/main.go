298b86b36570808ce52220c3ed19f1f2da323910
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-04-25 13:35:27 -0300

    Create ex005

---
+++ 07_ninja-level-2/ex005/main.go
@@ -0,0 +1,17 @@
+/*
+Create a variable of type string using a raw string literal. Print it.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	x := `Hello,
+	WORLD
+	!
+
+
+	:)`
+	fmt.Println(x)
+}
