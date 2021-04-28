a5330d114bbb65e86cfdfec5cd96bc52a8dfc041
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-04-28 11:50:51 -0300

    Create ex006

---
+++ 09_ninja-level-3/ex006/main.go
@@ -0,0 +1,15 @@
+/*
+Create a program that shows the “if statement” in action.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	n := 2021
+	if n == 2021 {
+		fmt.Println("Parece que n é igual a 2021 :o")
+
+	}
+}
